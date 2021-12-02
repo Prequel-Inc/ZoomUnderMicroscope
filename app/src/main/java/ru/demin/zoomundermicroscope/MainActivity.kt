54ad82859b4f385b5be2dc3dc7bace0114e3f538
Aleksei Demin <a.demin@prequelapp.com>
2021-12-02 16:54:48 +0300

    handle touch and scale same view = trembling while zoom

--- app/src/main/java/ru/demin/zoomundermicroscope/MainActivity.kt
+++ app/src/main/java/ru/demin/zoomundermicroscope/MainActivity.kt
@@ -3,6 +3,8 @@ package ru.demin.zoomundermicroscope
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.ScaleGestureDetector
 import com.google.android.exoplayer2.ExoPlayer
 import com.google.android.exoplayer2.MediaItem
 import com.google.android.exoplayer2.Player
@@ -11,11 +13,37 @@ import kotlinx.android.synthetic.main.activity_main.*
 class MainActivity : AppCompatActivity() {
 
     private val exoPlayer by lazy { ExoPlayer.Builder(this).build() }
+    private val scaleGestureDetector by lazy {
+        ScaleGestureDetector(this, object : ScaleGestureDetector.OnScaleGestureListener {
+            var totalScale = 1f
+
+            override fun onScale(detector: ScaleGestureDetector): Boolean {
+                totalScale *= detector.scaleFactor
+                Log.d("Povarity", "totalScale = $totalScale scaleFactor = ${detector.scaleFactor}")
+                player_view.scale(totalScale)
+                return true
+            }
+
+            override fun onScaleBegin(detector: ScaleGestureDetector): Boolean {
+                player_view.pivotX = detector.focusX
+                player_view.pivotY = detector.focusY
+                return true
+            }
+
+            override fun onScaleEnd(detector: ScaleGestureDetector) = Unit
+        })
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        setupPlayer()
+//        view_touch_handler.setOnTouchListener { _, event -> scaleGestureDetector.onTouchEvent(event) }
+        player_view.setOnTouchListener { _, event -> scaleGestureDetector.onTouchEvent(event) }
+    }
+
+    private fun setupPlayer() {
         player_view.player = exoPlayer
         exoPlayer.run {
             addMediaItem(MediaItem.fromUri(VIDEO_URI))
---
+++ app/src/main/java/ru/demin/zoomundermicroscope/ViewUtils.kt
@@ -0,0 +1,8 @@
+package ru.demin.zoomundermicroscope
+
+import android.view.View
+
+fun View.scale(scale: Float) {
+    scaleX = scale
+    scaleY = scale
+}
